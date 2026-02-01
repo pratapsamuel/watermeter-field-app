d5be49575e84663a8d65e7873ddb51ff826c8113
pratapsamuel <121438578+pratapsamuel@users.noreply.github.com>
2026-02-01 23:27:45 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "meter-camera-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json"
+];
+
+// Install: cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch: serve from cache if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
